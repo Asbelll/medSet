fd17ac4dae308ecc1df874c33c0f000e9d5d74b6
Chrystian Andrade <chrystian_alex@hotmail.com>
2024-05-31 11:59:04 -0300

    Implements KivyMD and ScreenManager Creates a simple app that is only visual for now, few trash lines on the database meant for testing.

--- database.py
+++ database.py
@@ -33,6 +33,38 @@ cur.execute("""CREATE TABLE IF NOT EXISTS plan (
         FOREIGN KEY (idMedicine) REFERENCES medicine(idMedicine)    
     )""")
 
+#Testing insert and database
+#Table order
+#User:
+#0 id
+#1 name
+#2 birth
+
+#medicine
+#0 id
+#1 name
+#2 dose
+#3 FK idUser
+
+#plan
+#0 id
+#1 days
+#2 time
+#3 FK idMedicine
+
+#If needs to search, use 'where' clause
+
+"""things = [
+    ('login', 2),
+    ('register', 3),
+    ('teste3', 4),
+    ('teste4', 5)
+]
+
+cur.executemany("INSERT INTO user (name, birth) VALUES (?,?)", things)
+cur.execute("SELECT * FROM user")
+print(cur.fetchall())"""
+
 #Commits command
 con.commit()
 
--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 import sqlite3
 import kivy
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
-from kivy.core.window import Window
+import kivymd
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
+from kivymd.uix.pickers import MDModalDatePicker
 
 kivy.require('1.9.0')
 
-Window.clearcolor = (0.5,0.5,0.5)
-Window.size = (360,600)
-con = sqlite3.connect('medset.db')
-cur = con.cursor()
+class Login(Screen):
+    pass
 
-class myClass(BoxLayout):
+class Register(Screen):
+    pass
 
-    def __init__(self):
-        super(myClass, self).__init__()
-
-    def change_label(self):
-        self.ids.input_label.text = str(self.ids.input_text.text)
-
-class medSet(App):
+class medset(MDApp):
     def build(self):
-        return myClass()
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Aliceblue"
+        Builder.load_file('medset.kv')
+
+        sm = ScreenManager()
+        sm.add_widget(Login(name = 'login'))
+        sm.add_widget(Register(name = 'register'))
+        return sm
     
-ms = medSet()
-ms.run()
+    def on_ok(self, instance_date_picker):
+        instance_date_picker.dismiss()
+        self.root.get_screen('register').ids.birthday.text = str(instance_date_picker.get_date()[0])
+
+    def showCalendar(self, focus):
+        if not focus:
+            return
+        
+        date_dialog = MDModalDatePicker()
+        date_dialog.bind(on_ok=self.on_ok)
+        date_dialog.open()
+        print(self.root.current_screen)
+    
+medset().run()
